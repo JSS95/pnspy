cfa420f1db699fa70a70a96921f6116bc8bc7824
Jisoo Song <jeesoo9595@snu.ac.kr>
2025-12-12 22:10:35 +0900

    feat: add reconstruction
    Add Reconstruct() and reconstruct()

--- src/pns/transform.py
+++ src/pns/transform.py
@@ -9,6 +9,8 @@ __all__ = [
     "project",
     "Embed",
     "embed",
+    "Reconstruct",
+    "reconstruct",
 ]
 
 
@@ -223,3 +225,108 @@ def embed(x, v, r):
     ... ax2.set_aspect("equal")
     """
     return _embed(x, v, r)
+
+
+class Reconstruct:
+    r"""Reconstruct data on a low-dimensional unit hypersphere. to a sub-hypersphere.
+
+    Parameters
+    ----------
+    [op]_func : callable
+        Operator function.
+    dtype : type
+        Data type.
+
+    Notes
+    -----
+    The purpose of this class is to facilitate defining computational graphs,
+    without writing the same function multiple times for different frameworks.
+
+    The instance of this class is the function
+    :math:`f^{-1}_k: S^{d-k} \to A_{d-k}(v_k, r_k) \subset S^{d-k+1}` for
+    :math:`k = 1, 2, \ldots, d-1` in the original paper.
+    Here, :math:`A_{d-k}(v_k, r_k)` is a subsphere of the hypersphere :math:`S^{d-k+1}`.
+    The input is :math:`x^\dagger \in S^{m-1} \subset \mathbb{R}^{m}`
+    and the output is :math:`x \in S^m \subset \mathbb{R}^{m+1}`,
+    where :math:`m = d-k+1`.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from pns.transform import Reconstruct
+    >>> t = np.linspace(0, 2 * np.pi, 100)
+    >>> x = np.vstack((np.cos(t), np.sin(t))).T
+    >>> reconstruct = Reconstruct(np.multiply, np.sin, np.cos, np.full, np.hstack)
+    >>> x_rec = reconstruct(x, np.array([0, 0, 1]), 0.5)
+    """
+
+    # TODO: replace full_func and hstack_func
+    def __init__(
+        self,
+        mul_func,
+        sin_func,
+        cos_func,
+        full_func,
+        hstack_func,
+        dtype=np.float64,
+    ):
+        self.mul = mul_func
+        self.sin = sin_func
+        self.cos = cos_func
+        self.full = full_func
+        self.hstack = hstack_func
+        self.dtype = dtype
+
+    def __call__(self, x, v, r):
+        R = rotation_matrix(v)
+        vec = self.hstack(
+            [self.sin(r) * x, self.full(len(x), self.cos(r)).reshape(-1, 1)]
+        )
+        return vec @ R
+
+
+_reconstruct = Reconstruct(
+    np.multiply,
+    np.sin,
+    np.cos,
+    np.full,
+    np.hstack,
+)
+
+
+def reconstruct(x, v, r):
+    r"""Numpy-compatible instance of :class:`Reconstruct`.
+
+    Parameters
+    ----------
+    x : (N, m) real array
+        Data :math:`x^\dagger` on a low-dimensional unit hypersphere :math:`S^{m-1}`.
+    v : (m+1,) real array
+        Sub-hypersphere axis.
+    r : scalar
+        Sub-hypersphere geodesic distance.
+
+    Returns
+    -------
+    (N, m+1) real array
+        Data :math:`x \in A_{m-1} \subset S^m \subset \mathbb{R}^{m+1}`,
+        on a subsphere :math:`A_{m-1}` of a unit hypersphere :math:`S^m`.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from pns.transform import reconstruct
+    >>> from pns.util import unit_sphere
+    >>> t = np.linspace(0, 2 * np.pi, 100)
+    >>> x = np.vstack((np.cos(t), np.sin(t))).T
+    >>> x_rec = reconstruct(x, np.array([0, 0, 1]), 0.5)
+    >>> import matplotlib.pyplot as plt  # doctest: +SKIP
+    ... fig = plt.figure()
+    ... ax1 = fig.add_subplot(121)
+    ... ax1.scatter(*x.T)
+    ... ax1.set_aspect("equal")
+    ... ax2 = fig.add_subplot(122, projection='3d', computed_zorder=False)
+    ... ax2.plot_surface(*unit_sphere(), color='skyblue', alpha=0.6, edgecolor='gray')
+    ... ax2.scatter(*x_rec.T)
+    """
+    return _reconstruct(x, v, r)
