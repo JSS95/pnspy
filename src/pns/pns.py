f2c5c44fd3eb7bafffee871cb9e3d2e7885b3a83
Jisoo Song <jeesoo9595@snu.ac.kr>
2025-12-13 11:38:57 +0900

    feat: implement PNS
    Add file
    Modify pss()

---
+++ src/pns/pns.py
@@ -0,0 +1,15 @@
+"""Detect principal nested spheres using :func:`pns.pss`."""
+
+__all__ = [
+    "PNS",
+]
+
+
+class PNS:
+    """Principal nested spheres analysis.
+
+    Notes
+    -----
+    The purpose of this class is to facilitate defining computational graphs,
+    without writing the same function multiple times for different frameworks.
+    """
