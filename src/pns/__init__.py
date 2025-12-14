759d03f55362b96e9b5aad6d4ced3710d8afee4f
Jisoo Song <jeesoo9595@snu.ac.kr>
2025-12-15 00:36:14 +0900

    refact: move import location of some objects

--- src/pns/__init__.py
+++ src/pns/__init__.py
@@ -7,10 +7,13 @@
 import numpy as np
 
 from .pss import pss
-from .transform import embed, project
+from .transform import embed, project, reconstruct
 
 __all__ = [
     "pns",
+    "ExtrinsicPNS",
+    "extrinsic_pns",
+    "inverse_extrinsic_pns",
 ]
 
 
@@ -88,3 +91,124 @@ def pns(x, n_components, tol=1e-3, maxiter=None, lm_kwargs=None):
         rs[i] = r
         xis[:, [i]] = xi
     return vs, rs, xis, x
+
+
+class ExtrinsicPNS:
+    r"""Transform data to low-dimensional hypersphere in extrinsic coordinates.
+
+    Parameters
+    ----------
+    project_func : pns.transform.Project
+    embed_func : pns.transform.Embed
+    reconstruct_func : pns.transform.Reconstruct
+    dtype : type
+        Data type for intermediate vectors.
+
+    Examples
+    --------
+    >>> from pns import ExtrinsicPNS
+    >>> from pns.pss import pss
+    >>> from pns.transform import project, embed, reconstruct
+    >>> from pns.util import circular_data
+    >>> x = circular_data([0, -1, 0]).reshape(-1, 3)
+    >>> v, r = pss(x)
+    >>> extrinsic_pns = ExtrinsicPNS(project, embed, reconstruct)
+    >>> x_transformed = extrinsic_pns(x, [v], [r])
+    """
+
+    def __init__(self, project_func, embed_func, reconstruct_func, dtype=np.float64):
+        self.project = project_func
+        self.embed = embed_func
+        self.reconstruct = reconstruct_func
+        self.dtype = dtype
+
+    def __call__(self, X, vs, rs, lastop_kwargs=None):
+        for i, (v, r) in enumerate(zip(vs, rs)):
+            v, r = v.astype(self.dtype), r.reshape(1).astype(self.dtype)
+            P, _ = self.project(X, v, r)
+            if i < len(vs) - 1:
+                X = self.embed(P, v, r)
+            else:
+                X = self.embed(P, v, r, lastop_kwargs)
+        return X
+
+    def inverse(self, x, vs, rs, lastop_kwargs=None):
+        """Inverse transformation of ``self(X, vs, rs)``."""
+        for i, (v, r) in enumerate(zip(reversed(vs), reversed(rs))):
+            if i < len(vs) - 1:
+                x = self.reconstruct(x, v, r)
+            else:
+                x = self.reconstruct(x, v, r, lastop_kwargs)
+        return x
+
+
+_extrinsic_pns = ExtrinsicPNS(project, embed, reconstruct)
+
+
+def extrinsic_pns(X, vs, rs, *args, **kwargs):
+    r"""Numpy-compatible instance of :class:`ExtrinsicPNS`.
+
+    Parameters
+    ----------
+    X : (N, d+1) real array
+        Extrinsic coordinates of data on a ``d``-dimensional hypersphere,
+        embedded in a ``d+1``-dimensional space.
+    vs : list of (m+1,) real arrays
+        Subsphere axes.
+    rs : list of scalars
+        Subsphere geodesic distances.
+
+    Returns
+    -------
+    (N, d-k+1) real array
+        Extrinsic coordinates of data on a low-dimensional unit hypersphere.
+
+    Examples
+    --------
+    >>> from pns import extrinsic_pns
+    >>> from pns.pss import pss
+    >>> from pns.util import circular_data
+    >>> x = circular_data([0, -1, 0]).reshape(-1, 3)
+    >>> v, r = pss(x)
+    >>> x_transformed = extrinsic_pns(x, [v], [r])
+    >>> import matplotlib.pyplot as plt  # doctest: +SKIP
+    ... plt.scatter(*x_transformed.reshape(-1, 2).T)
+    """
+    return _extrinsic_pns(X, vs, rs, *args, **kwargs)
+
+
+def inverse_extrinsic_pns(x, vs, rs, *args, **kwargs):
+    """Inverse of :func:`extrinsic_pns`.
+
+    Parameters
+    ----------
+    x : (N, d-k+1) real array
+        Extrinsic coordinates of data on a low-dimensional unit hypersphere.
+    vs : list of (m+1,) real arrays
+        Subsphere axes.
+    rs : list of scalars
+        Subsphere geodesic distances.
+
+    Returns
+    -------
+    (N, d+1) real array
+        Extrinsic coordinates of data on a ``d``-dimensional hypersphere,
+        embedded in a ``d+1``-dimensional space.
+
+    Examples
+    --------
+    >>> from pns import inverse_extrinsic_pns, extrinsic_pns
+    >>> from pns.pss import pss
+    >>> from pns.util import circular_data, unit_sphere
+    >>> x = circular_data([0, -1, 0]).reshape(-1, 3)
+    >>> v, r = pss(x)
+    >>> x_transformed = extrinsic_pns(x, [v], [r])
+    >>> x_reconstructed = inverse_extrinsic_pns(x_transformed, [v], [r])
+    >>> import matplotlib.pyplot as plt  # doctest: +SKIP
+    ... fig = plt.figure()
+    ... ax = fig.add_subplot(projection='3d', computed_zorder=False)
+    ... ax.plot_surface(*unit_sphere(), color='skyblue', alpha=0.6, edgecolor='gray')
+    ... ax.scatter(*x.T, marker=".", zorder=10)
+    ... ax.scatter(*x_reconstructed.T, marker="x", zorder=10)
+    """
+    return _extrinsic_pns.inverse(x, vs, rs, *args, **kwargs)
